8d77382cc21ce5fff01b477af67d1a89eb74014a
kvoon <kvoon@duck.com>
2025-08-25 10:49:59 +0800

    fix: don't pass token when it not exist

--- src/shared.ts
+++ src/shared.ts
@@ -4,44 +4,55 @@ import md5 from 'md5'
 
 interface V1Query {
   'app_id': string
-  'access-token': string
+  'access-token'?: string
   'et': string
   'sign': string
 }
+
 interface V2Query {
   appid: string
-  token: string
+  token?: string
   et: string
   sign: string
 }
 
 export function v1Query(app_id: string, key: string): V1Query {
-  const access_token = localStorage.getItem('token') || ''
   const timestamp = Date.now() || -1
   const et = Math.floor(timestamp / 1000)
   const app_sign = md5(`${et}${key}`)
 
-  return {
-    'app_id': app_id,
-    'access-token': access_token,
-    'et': String(et),
-    'sign': app_sign,
+  const res: V1Query = {
+    app_id,
+    et: String(et),
+    sign: app_sign,
   }
+
+  const access_token = localStorage.getItem('token')
+
+  if (access_token)
+    res['access-token'] = access_token
+
+  return res
 }
 
 export function v2Query(app_id: string, key: string): V2Query {
-  const access_token = localStorage.getItem('token') || ''
   const timestamp = Date.now() || -1
   const et = Math.floor(timestamp / 1000)
   const app_sign = md5(`${et}${key}`)
   const app_sign_v2 = getMd5Middle8Chars(app_sign)
 
-  return {
+  const res: V2Query = {
     appid: app_id,
-    token: access_token,
     et: String(et),
     sign: app_sign_v2,
   }
+
+  const token = localStorage.getItem('token')
+
+  if (token)
+    res.token = token
+
+  return res
 }
 
 export function getMd5Middle8Chars(md5: string): string {
